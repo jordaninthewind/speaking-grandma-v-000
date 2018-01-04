91bf053db8e031b7837a9e60ef53d58e0c4f83b4
Jordan Kline <jordan.kline@gmail.com>
2018-01-04 16:34:53 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(words)
   if words == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif words == words.uppercase
+  elsif words == words.upcase
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
